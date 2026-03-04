26123cc32f5f49761eeff857440f034029a2c766
gurkvatten <johanwilhelmkarlsson@gmail.com>
2026-03-04 10:25:03 +0100

    First init added driver entity

---
+++ src/main/java/org/example/kjulab1/KjuLab1Application.java
@@ -0,0 +1,13 @@
+package org.example.kjulab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjuLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjuLab1Application.class, args);
+    }
+
+}
